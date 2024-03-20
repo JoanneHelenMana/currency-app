659bcd5423f1609f76b78c9b830bd04fad29ba20
JoanneHelenMana <joannehmana@gmail.com>
2024-03-20 12:00:12 +0800

    added model

---
+++ currency-app/Models/CurrencyModel.cs
@@ -0,0 +1,75 @@
+﻿namespace currency_app
+{
+    /// <summary>
+    /// The model stores amounts of coins for each value and the total amount of money held in the 'wallet'.
+    /// </summary>
+    internal class CurrencyModel
+    {
+        int dollar = 0;
+        public int Dollar 
+        { 
+            get 
+            {
+                return dollar;
+            }
+            set
+            {
+                dollar = value;
+                if (dollar < 0) dollar = 0;
+            }                
+        }
+
+        int fiftyCent = 0;
+        public int FiftyCent
+        {
+            get
+            {
+                return fiftyCent;
+            }
+            set
+            {
+                fiftyCent = value;
+                if (fiftyCent < 0) fiftyCent = 0;
+            }
+        }
+
+        int tenCent = 0;
+        public int TenCent
+        {
+            get
+            {
+                return tenCent;
+            }
+            set
+            {
+                tenCent = value;
+                if (tenCent < 0) tenCent = 0;
+            }
+        }
+
+        int fiveCent = 0;
+        public int FiveCent
+        {
+            get
+            {
+                return fiveCent;
+            }
+            set
+            {
+                fiveCent = value;
+                if (fiveCent < 0) fiveCent = 0;
+            }
+        }
+
+        double fiftyCentValue = 0.5;
+        double tenCentValue = 0.1;
+        double fiveCentValue = 0.5;
+        public double TotalAmount
+        {
+            get
+            {
+                return dollar + (fiftyCent * fiftyCentValue) + (tenCent * tenCentValue) + (fiveCent * fiveCentValue);
+            }
+        }
+    }
+}
